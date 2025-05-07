43c28fb4a23da274066d1b5e1704382d19b8ffa8
Luiz Paulo Rodrigues <lprodrigs@gmail.com>
2025-05-07 12:05:51 -0300

    Update AnthropicConfig.kt

--- src/main/kotlin/com/extrabox/periodization/config/AnthropicConfig.kt
+++ src/main/kotlin/com/extrabox/periodization/config/AnthropicConfig.kt
@@ -21,7 +21,7 @@ class AnthropicConfig {
     @Bean
     fun okHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
-            .connectTimeout(1, TimeUnit.MINUTES)
+            .connectTimeout(5, TimeUnit.MINUTES)
             .readTimeout(5, TimeUnit.MINUTES)
             .writeTimeout(1, TimeUnit.MINUTES)
             .build()
